91e938ac4c42a0fe75c4d304580e7820581fdb02
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-08-11 18:43:34 -0300

    some fixes

--- app/simple-auth-jwt/server.py
+++ app/simple-auth-jwt/server.py
@@ -56,6 +56,7 @@ def get_userinfo(token):
 
 def create_headers(headers = {}):
 	headers['Timestamp'] = datetime.now(tz=timezone.utc).timestamp()
+	headers['Content-Type'] = 'application/json'
 
 	return headers
 	
