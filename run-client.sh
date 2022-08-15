6d7f9108d053196df4f856c4d0f5cf1c8dac8f49
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-08-15 00:36:19 -0300

    improve run scripts

--- run-client.sh
+++ run-client.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-python3 app/$1/client.py
+
+arr=( ping example hello body sum crud currency auth auth-jwt)
+
+if [ -z "$1" ]; then
+    printf "#\n# %s" "API name are expected! Try one of available options:"
+    printf '\n#\t%s' "${arr[@]}"
+    printf '\n#'
+fi
+
+if [[ "${arr[*]}" =~ "${1}" ]]; then
+    python3 app/$1/client.py
+else
+    printf "#\n# %s" "API not found! Try one of available options:"
+    printf '\n#\t%s' "${arr[@]}"
+    printf '\n#'
+fi
--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-export FLASK_APP=app/$1/server && export FLASK_DEBUG=1 && flask run --reload --host=0.0.0.0 --debugger
+
+arr=( ping example hello body sum crud currency auth auth-jwt)
+
+if [ -z "$1" ]; then
+    printf "#\n# %s" "API name are expected! Try one of available options:"
+    printf '\n#\t%s' "${arr[@]}"
+    printf '\n#'
+fi
+
+if [[ "${arr[*]}" =~ "${1}" ]]; then
+    export FLASK_APP=app/$1/server && export FLASK_DEBUG=1 && flask run --reload --host=0.0.0.0 --debugger
+else
+    printf "#\n# %s" "API $1 not found! Try one of available options:"
+    printf '\n#\t%s' "${arr[@]}"
+    printf '\n#'
+fi
