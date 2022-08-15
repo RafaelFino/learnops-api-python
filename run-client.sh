5ef44a30c48e69fb28efb21fba36cbad8cc2d4b5
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2022-08-15 00:43:38 -0300

    improve run scripts

--- run-client.sh
+++ run-client.sh
@@ -6,12 +6,14 @@ if [ -z "$1" ]; then
     printf "#\n# %s" "API name are expected! Try one of available options:"
     printf '\n#\t%s' "${arr[@]}"
     printf '\n#'
+    exit 1
 fi
 
-if [[ "${arr[*]}" =~ "${1}" ]]; then
-    python3 app/$1/client.py
-else
+if ! [ -f app/$1/client.py ]; then    
     printf "#\n# %s" "API not found! Try one of available options:"
     printf '\n#\t%s' "${arr[@]}"
     printf '\n#'
+    exit 1
 fi
+
+python3 app/$1/client.py
--- run-server.sh
+++ run-server.sh
@@ -6,12 +6,14 @@ if [ -z "$1" ]; then
     printf "#\n# %s" "API name are expected! Try one of available options:"
     printf '\n#\t%s' "${arr[@]}"
     printf '\n#'
+    exit 1
 fi
 
-if [[ "${arr[*]}" =~ "${1}" ]]; then
-    export FLASK_APP=app/$1/server && export FLASK_DEBUG=1 && flask run --reload --host=0.0.0.0 --debugger
-else
-    printf "#\n# %s" "API $1 not found! Try one of available options:"
+if ! [ -f app/$1/server.py ]; then    
+    printf "#\n# %s" "API not found! Try one of available options:"
     printf '\n#\t%s' "${arr[@]}"
     printf '\n#'
+    exit 1
 fi
+
+export FLASK_APP=app/$1/server && export FLASK_DEBUG=1 && flask run --reload --host=0.0.0.0 --debugger
